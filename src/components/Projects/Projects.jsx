06300c72ca51262e2a536bc69efbcfb551a0c279
Travis <arma3cloud@gmail.com>
2022-08-15 15:15:39 -0700

    Update portfolio

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,20 +7,16 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
   const projects = [
-    { project_id: 'one', project_name: 'Discord Landing Page', short_desc: 'A clone design of discords landing page with some changes that I thought would look better.', display_img: 'images/Discord.webp', links: { github: 'https://github.com/TravisDevLife/discord-landing-page', website: 'https://discordlp.herokuapp.com/', purchase: '' } },
-    { project_id: 'two', project_name: 'RPS API', short_desc: 'An API using MERN Stack allowing devs to post/update devblogs with sections to Four Website.', display_img: 'images/API.webp', links: { github: '', website: 'https://i.gyazo.com/ef956ddfde9be986a5bb01b755c034cd.mp4', purchase: '' } },
-    { project_id: 'three', project_name: 'Bookmark Landing Page', short_desc: 'A landing page designed in Figma and brought to light using HTML & CSS.', display_img: 'images/Bookmark.webp', links: { github: 'https://github.com/TravisDevLife/bookmark-landing', website: 'https://bookmarklandingp.netlify.app/', purchase: '' } },
-    { project_id: 'four', project_name: 'Stats Website', short_desc: 'An advanced PHP statistics website for Rust Servers. Product is currently for sale on multiple sites and has grossed over $500.', display_img: 'images/stats.webp', links: { github: '', website: 'https://i.gyazo.com/c9dd9fe4b11f1114df818599a311462c.mp4', purchase: 'https://codefling.com/plugins/better-statistics' } },
-    { project_id: 'five', project_name: 'Four Website', short_desc: 'This website is for a Game Studio, made in React and uses my MERN API.', display_img: 'images/four.webp', links: { github: 'https://github.com/TravisDevLife/four', website: 'https://fourgamee.netlify.app/', purchase: '' } },
-    { project_id: 'six', project_name: 'Tip Calculator', short_desc: 'A website that generates a tip based off a few input fields, made in React.', display_img: 'images/tipcalc.webp', links: { github: 'https://github.com/TravisDevLife/tip-calculator', website: 'https://tipcalculatorr.herokuapp.com/', purchase: '' } },
+    { project_id: 'one', project_name: 'Travcord', short_desc: 'Tested my skills by recreating the Discord Landing Page, renamed site and logos for IP reasons.', display_img: 'images/Travcord.webp', links: { github: 'https://github.com/TravisDevLife/travcord', website: 'https://travcord.netlify.app/', purchase: '' } },
+    { project_id: 'two', project_name: 'Bookmark', short_desc: 'A landing page designed in Figma and brought to light using HTML & CSS.', display_img: 'images/Bookmark.webp', links: { github: 'https://github.com/TravisDevLife/bookmark-landing', website: 'https://bookmarklandingp.netlify.app/', purchase: '' } },
+    { project_id: 'three', project_name: 'RPS API', short_desc: 'An API using MERN Stack allowing devs to post/update devblogs with sections to Four Website.', display_img: 'images/API.webp', links: { github: '', website: 'https://i.gyazo.com/ef956ddfde9be986a5bb01b755c034cd.mp4', purchase: '' } },
+    { project_id: 'four', project_name: 'Misfits Boxing', short_desc: 'A website built for a boxing promotion company, allowing users to vote on the fighters that are on the card.', display_img: 'images/MisfitsBoxing.webp', links: { github: '', website: 'https://misfitsboxing.netlify.app/', purchase: '' } },
+    { project_id: 'five', project_name: 'Stats Website', short_desc: 'An advanced PHP statistics website for Rust Servers. Product is currently for sale on multiple sites and has grossed over $500.', display_img: 'images/stats.webp', links: { github: '', website: 'https://i.gyazo.com/c9dd9fe4b11f1114df818599a311462c.mp4', purchase: 'https://codefling.com/plugins/better-statistics' } },
+    { project_id: 'six', project_name: 'Four Website', short_desc: 'This website is for a Game Studio, made in React and uses my MERN API.', display_img: 'images/four.webp', links: { github: 'https://github.com/TravisDevLife/four', website: 'https://fourgamee.netlify.app/', purchase: '' } },
   ];
 
   useEffect(() => {
     gsap.fromTo(".my-projects-header", {x: 500, opacity: 0}, { x: 0, opacity: 1, duration: 1, scrollTrigger: {
-      trigger: ".my-projects-header"
-    } });
-
-    gsap.fromTo(".warning-notice", {opacity: 0, x: 400}, {opacity: 1, x: 0, duration: 1, scrollTrigger: {
       trigger: ".my-projects-header"
     } });
 
@@ -52,7 +48,6 @@ const Projects = () => {
   return (
     <div className='projects-wrapper' id='projects'>
         <h1 className='my-projects-header'>My Projects</h1>
-        <p className='warning-notice'>(Ran on heroku, may be slow loading)</p>
         <div className='projects-grid'>
           { 
             projects.map((item) => (

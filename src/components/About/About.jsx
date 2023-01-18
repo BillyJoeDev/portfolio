34f4e48985cbfe40b2a135538c22be8371c0b6ee
Billy Joe <arma3cloud@gmail.com>
2023-01-17 20:10:34 -0700

    Update portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,72 +1,117 @@
-import React, {useEffect} from 'react'
-import './About.css'
-import { FaPhp, FaNodeJs, FaMicrosoft, FaReact, FaDatabase } from 'react-icons/fa'
-import { DiMongodb } from 'react-icons/di'
-import { SiExpress } from 'react-icons/si'
-import gsap from 'gsap'
-import {ScrollTrigger} from 'gsap/ScrollTrigger'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useEffect } from 'react';
+import { DiMongodb } from 'react-icons/di';
+import {
+	FaDatabase,
+	FaMicrosoft,
+	FaNodeJs,
+	FaPhp,
+	FaReact,
+} from 'react-icons/fa';
+import { SiExpress } from 'react-icons/si';
+import './About.css';
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
-  useEffect(() => {
-    gsap.fromTo(".about-me-header", {scaleY: 0, scaleX: 0}, { scaleY: 1, scaleX: 1, duration: 0.5, scrollTrigger: {
-      trigger: ".about-me-header"
-    } });
+	useEffect(() => {
+		gsap.fromTo(
+			'.about-me-header',
+			{ scaleY: 0, scaleX: 0 },
+			{
+				scaleY: 1,
+				scaleX: 1,
+				duration: 0.5,
+				scrollTrigger: {
+					trigger: '.about-me-header',
+				},
+			}
+		);
 
-    gsap.fromTo(".who-is-section", {opacity: 0, x: -300}, {opacity: 1, x: 0, duration: 1, scrollTrigger: {
-      trigger: ".who-is-section"
-    } });
+		gsap.fromTo(
+			'.who-is-section',
+			{ opacity: 0, x: -300 },
+			{
+				opacity: 1,
+				x: 0,
+				duration: 1,
+				scrollTrigger: {
+					trigger: '.who-is-section',
+				},
+			}
+		);
 
-    gsap.fromTo(".about-section", {opacity: 0, x: 300}, {opacity: 1, x: 0, duration: 1, scrollTrigger: {
-      trigger: ".about-section"
-    } });
-  }, []);
+		gsap.fromTo(
+			'.about-section',
+			{ opacity: 0, x: 300 },
+			{
+				opacity: 1,
+				x: 0,
+				duration: 1,
+				scrollTrigger: {
+					trigger: '.about-section',
+				},
+			}
+		);
+	}, []);
 
-  return (
-    <article className='about-wrapper' id='about'>
-        <h1 className='about-me-header'>About Me</h1>
-        <div className='about-section-wrapper'>
-          <div className='who-is-section'>
-            <h1>Who is Travis?</h1>
-            <img src='images/about-image.svg' alt="Guy at desk looking at laptop next to window at night time" />
-          </div>
-          <div className='about-section'>
-            <p>I specialize in frontend development, although love to do backend work when I can. I have done work in software development, front-end/back-end web, database/server management, graphic design, and video game development. I love helping out developers where I can, and currently am a Code Reviewer for a website known as Codefling.com. I make sure that all developers are pushing through fast and responsive code as well as providing feedback where I can.</p>
-            <h1>My Skills:</h1>
-            <div className='skills-section'>
-              <div className='skill-container'>
-                <FaReact className='skill'/>
-                <h1>REACT</h1>
-              </div>
-              <div className='skill-container'>
-                <FaNodeJs className='skill'/>
-                <h1>JAVAS</h1>
-              </div>
-              <div className='skill-container'>
-                <SiExpress className='skill'/>
-                <h1>EXPRESS</h1>
-              </div>
-              <div className='skill-container'>
-                <FaMicrosoft className='skill'/>
-                <h1>.NET</h1>
-              </div>
-              <div className='skill-container'>
-                <DiMongodb className='skill'/>
-                <h1>MONGODB</h1>
-              </div>
-              <div className='skill-container'>
-                <FaDatabase className='skill'/>
-                <h1>MYSQL</h1>
-              </div>
-              <div className='skill-container'>
-                <FaPhp className='skill'/>
-                <h1>PHP</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-    </article>
-  )
-}
+	return (
+		<article className="about-wrapper" id="about">
+			<h1 className="about-me-header">About Me</h1>
+			<div className="about-section-wrapper">
+				<div className="who-is-section">
+					<h1>Who is Travis?</h1>
+					<img
+						src="images/about-image.svg"
+						alt="Guy at desk looking at laptop next to window at night time"
+					/>
+				</div>
+				<div className="about-section">
+					<p>
+						I specialize in frontend development, although love to do backend
+						work when I can. I have done work in software development industry
+						for around 6+ years. Started off with database/server management,
+						then ventured into game development and finally I have found my love
+						for Web Development. I love helping out developers where I can, and
+						currently am a Code Reviewer for a website known as Codefling.com. I
+						make sure that all developers are pushing through fast and
+						responsive code as well as providing feedback where I can.
+					</p>
+					<h1>My Skills:</h1>
+					<div className="skills-section">
+						<div className="skill-container">
+							<FaReact className="skill" />
+							<h1>REACT</h1>
+						</div>
+						<div className="skill-container">
+							<FaNodeJs className="skill" />
+							<h1>JAVAS</h1>
+						</div>
+						<div className="skill-container">
+							<SiExpress className="skill" />
+							<h1>EXPRESS</h1>
+						</div>
+						<div className="skill-container">
+							<FaMicrosoft className="skill" />
+							<h1>.NET</h1>
+						</div>
+						<div className="skill-container">
+							<DiMongodb className="skill" />
+							<h1>MONGODB</h1>
+						</div>
+						<div className="skill-container">
+							<FaDatabase className="skill" />
+							<h1>MYSQL</h1>
+						</div>
+						<div className="skill-container">
+							<FaPhp className="skill" />
+							<h1>PHP</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</article>
+	);
+};
 
-export default About
+export default About;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,76 +1,254 @@
-import React, {useEffect} from 'react'
-import { FaGithub, FaEye, FaShoppingCart } from 'react-icons/fa'
-import './Projects.css'
-import gsap from 'gsap'
-import {ScrollTrigger} from 'gsap/ScrollTrigger'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useEffect } from 'react';
+import { FaEye, FaGithub, FaShoppingCart } from 'react-icons/fa';
+import './Projects.css';
 gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
-  const projects = [
-    { project_id: 'one', project_name: 'Travcord', short_desc: 'Tested my skills by recreating the Discord Landing Page, renamed site and logos for IP reasons.', display_img: 'images/Travcord.webp', links: { github: 'https://github.com/TravisDevLife/travcord', website: 'https://travcord.netlify.app/', purchase: '' } },
-    { project_id: 'two', project_name: 'Bookmark', short_desc: 'A landing page designed in Figma and brought to light using HTML & CSS.', display_img: 'images/Bookmark.webp', links: { github: 'https://github.com/TravisDevLife/bookmark-landing', website: 'https://bookmarklandingp.netlify.app/', purchase: '' } },
-    { project_id: 'three', project_name: 'RPS API', short_desc: 'An API using MERN Stack allowing devs to post/update devblogs with sections to Four Website.', display_img: 'images/API.webp', links: { github: '', website: 'https://i.gyazo.com/ef956ddfde9be986a5bb01b755c034cd.mp4', purchase: '' } },
-    { project_id: 'four', project_name: 'Misfits Boxing', short_desc: 'A website built for a boxing promotion company, allowing users to vote on the fighters that are on the card.', display_img: 'images/MisfitsBoxing.webp', links: { github: '', website: 'https://misfitsboxing.netlify.app/', purchase: '' } },
-    { project_id: 'five', project_name: 'Stats Website', short_desc: 'An advanced PHP statistics website for Rust Servers. Product is currently for sale on multiple sites and has grossed over $500.', display_img: 'images/stats.webp', links: { github: '', website: 'https://i.gyazo.com/c9dd9fe4b11f1114df818599a311462c.mp4', purchase: 'https://codefling.com/plugins/better-statistics' } },
-    { project_id: 'six', project_name: 'Four Website', short_desc: 'This website is for a Game Studio, made in React and uses my MERN API.', display_img: 'images/four.webp', links: { github: 'https://github.com/TravisDevLife/four', website: 'https://fourgamee.netlify.app/', purchase: '' } },
-  ];
+	const projects = [
+		{
+			project_id: 'one',
+			project_name: 'Vanity Rust',
+			short_desc:
+				'This was a freelance project for a company, I decided to make this website in PHP as they didnt want a backend overhead.',
+			display_img: 'images/Vanity.webp',
+			links: {
+				github: '',
+				website: 'https://vanity-rust.com/',
+				purchase: '',
+			},
+		},
+		{
+			project_id: 'two',
+			project_name: 'RPS API',
+			short_desc:
+				'This was another freelance project for a game development company, they wanted a backend site that there developers could use to add/edit devblogs with thier work.',
+			display_img: 'images/API.webp',
+			links: {
+				github: '',
+				website: 'https://i.gyazo.com/ef956ddfde9be986a5bb01b755c034cd.mp4',
+				purchase: '',
+			},
+		},
+		{
+			project_id: 'three',
+			project_name: 'Travcord',
+			short_desc:
+				'This project was to test my Frontend skills by recreating the Discord landing page, had to rename for IP Reasons.',
+			display_img: 'images/Travcord.webp',
+			links: {
+				github: 'https://github.com/TravisDevLife/travcord',
+				website: 'https://travcord.netlify.app/',
+				purchase: '',
+			},
+		},
+		{
+			project_id: 'four',
+			project_name: 'Bookmark',
+			short_desc:
+				'A landing page designed in Figma and brought to light using HTML & CSS.',
+			display_img: 'images/Bookmark.webp',
+			links: {
+				github: 'https://github.com/TravisDevLife/bookmark-landing',
+				website: 'https://bookmarklandingp.netlify.app/',
+				purchase: '',
+			},
+		},
+		{
+			project_id: 'five',
+			project_name: 'Misfits Boxing',
+			short_desc:
+				'A website built in React for a boxing promotion company, allowing users to vote on the fighters that are on the active card.',
+			display_img: 'images/MisfitsBoxing.webp',
+			links: {
+				github: '',
+				website: 'https://misfitsboxing.netlify.app/',
+				purchase: '',
+			},
+		},
+		{
+			project_id: 'six',
+			project_name: 'Stats Website',
+			short_desc:
+				'An advanced PHP statistics website for Rust Servers. Product is currently for sale on multiple sites and has grossed over $500.',
+			display_img: 'images/stats.webp',
+			links: {
+				github: '',
+				website: 'https://i.gyazo.com/c9dd9fe4b11f1114df818599a311462c.mp4',
+				purchase: 'https://codefling.com/plugins/better-statistics',
+			},
+		},
+	];
 
-  useEffect(() => {
-    gsap.fromTo(".my-projects-header", {x: 500, opacity: 0}, { x: 0, opacity: 1, duration: 1, scrollTrigger: {
-      trigger: ".my-projects-header"
-    } });
+	useEffect(() => {
+		gsap.fromTo(
+			'.my-projects-header',
+			{ x: 500, opacity: 0 },
+			{
+				x: 0,
+				opacity: 1,
+				duration: 1,
+				scrollTrigger: {
+					trigger: '.my-projects-header',
+				},
+			}
+		);
 
-    gsap.fromTo(".project.one", {opacity: 0, x: -300}, {opacity: 1, x: 0, duration: 0.8, scrollTrigger: {
-      trigger: ".projects-grid"
-    } });
+		gsap.fromTo(
+			'.project.one',
+			{ opacity: 0, y: 300 },
+			{
+				opacity: 1,
+				y: 0,
+				duration: 0.8,
+				ease: 'power4.out',
+				scrollTrigger: {
+					trigger: '.projects-grid',
+				},
+			}
+		);
 
-    gsap.fromTo(".project.two", {opacity: 0, y: -300}, {opacity: 1, y: 0, duration: 0.8, scrollTrigger: {
-      trigger: ".projects-grid"
-    } });
+		gsap.fromTo(
+			'.project.two',
+			{ opacity: 0, y: 300 },
+			{
+				opacity: 1,
+				y: 0,
+				duration: 0.8,
+				ease: 'power4.out',
+				scrollTrigger: {
+					trigger: '.projects-grid',
+				},
+			}
+		);
 
-    gsap.fromTo(".project.three", {opacity: 0, x: 300}, {opacity: 1, x: 0, duration: 0.8, scrollTrigger: {
-      trigger: ".projects-grid"
-    } });
+		gsap.fromTo(
+			'.project.three',
+			{ opacity: 0, y: 300 },
+			{
+				opacity: 1,
+				y: 0,
+				duration: 0.8,
+				ease: 'power4.out',
+				scrollTrigger: {
+					trigger: '.projects-grid',
+				},
+			}
+		);
 
-    gsap.fromTo(".project.four", {opacity: 0, x: -300}, {opacity: 1, x: 0, duration: 0.8, scrollTrigger: {
-      trigger: ".projects-grid"
-    } });
+		gsap.fromTo(
+			'.project.four',
+			{ opacity: 0, y: 300 },
+			{
+				opacity: 1,
+				y: 0,
+				duration: 0.8,
+				ease: 'power4.out',
+				scrollTrigger: {
+					trigger: '.projects-grid',
+				},
+			}
+		);
 
-    gsap.fromTo(".project.five", {opacity: 0, y: 300}, {opacity: 1, y: 0, duration: 0.8, scrollTrigger: {
-      trigger: ".projects-grid"
-    } });
+		gsap.fromTo(
+			'.project.five',
+			{ opacity: 0, y: 300 },
+			{
+				opacity: 1,
+				y: 0,
+				duration: 0.8,
+				ease: 'power4.out',
+				scrollTrigger: {
+					trigger: '.projects-grid',
+				},
+			}
+		);
 
-    gsap.fromTo(".project.six", {opacity: 0, x: 300}, {opacity: 1, x: 0, duration: 0.8, scrollTrigger: {
-      trigger: ".projects-grid"
-    } });
-  }, []);
+		gsap.fromTo(
+			'.project.six',
+			{ opacity: 0, y: 300 },
+			{
+				opacity: 1,
+				y: 0,
+				duration: 0.8,
+				ease: 'power4.out',
+				scrollTrigger: {
+					trigger: '.projects-grid',
+				},
+			}
+		);
+	}, []);
 
-  return (
-    <div className='projects-wrapper' id='projects'>
-        <h1 className='my-projects-header'>My Projects</h1>
-        <div className='projects-grid'>
-          { 
-            projects.map((item) => (
-              <a href={item.links.website} target="_blank" key={item.project_id} >
-                <div className={'project ' + item.project_id} style={{ backgroundImage: 'linear-gradient(transparent 0%, rgba(18,18,18,1) 100%), url(' + item.display_img + ')'}}>
-                  <div className='project_summary'>
-                    <h1>{item.project_name}</h1>
-                    <p>{item.short_desc}</p>
-                  </div>
-                  { item.links.github == "" && item.links.website == "" && item.links.purchase == "" ? null :
-                    <div className='project-links'>
-                      { item.links.github != "" ? <a target="_blank" href={item.links.github} aria-label="Visit the github"><FaGithub className='icon github' aria-hidden="true" /></a> : null }
-                      { item.links.website != "" ? <a target="_blank" href={item.links.website} aria-label="Visit the website"><FaEye className='icon website' aria-hidden="true"/></a> : null }
-                      { item.links.purchase != "" ? <a target="_blank" href={item.links.purchase} aria-label="Visit the shop"><FaShoppingCart className='icon website' aria-hidden="true"/></a> : null }
-                    </div>
-                  }
-                </div>
-              </a>
-            ))
-          }
-        </div>
-    </div>
-  )
-}
+	return (
+		<div className="projects-wrapper" id="projects">
+			<h1 className="my-projects-header">My Projects</h1>
+			<div className="projects-grid">
+				{projects.map((item) => (
+					<a
+						href={item.links.website}
+						target="_blank"
+						key={item.project_id}
+						rel="noreferrer"
+					>
+						<div
+							className={'project ' + item.project_id}
+							style={{
+								backgroundImage:
+									'linear-gradient(transparent 0%, rgba(18,18,18,1) 100%), url(' +
+									item.display_img +
+									')',
+							}}
+						>
+							<div className="project_summary">
+								<h1>{item.project_name}</h1>
+								<p>{item.short_desc}</p>
+							</div>
+							{item.links.github == '' &&
+							item.links.website == '' &&
+							item.links.purchase == '' ? null : (
+								<div className="project-links">
+									{item.links.github != '' ? (
+										<a
+											target="_blank"
+											href={item.links.github}
+											aria-label="Visit the github"
+											rel="noreferrer"
+										>
+											<FaGithub className="icon github" aria-hidden="true" />
+										</a>
+									) : null}
+									{item.links.website != '' ? (
+										<a
+											target="_blank"
+											href={item.links.website}
+											aria-label="Visit the website"
+											rel="noreferrer"
+										>
+											<FaEye className="icon website" aria-hidden="true" />
+										</a>
+									) : null}
+									{item.links.purchase != '' ? (
+										<a
+											target="_blank"
+											href={item.links.purchase}
+											aria-label="Visit the shop"
+											rel="noreferrer"
+										>
+											<FaShoppingCart
+												className="icon website"
+												aria-hidden="true"
+											/>
+										</a>
+									) : null}
+								</div>
+							)}
+						</div>
+					</a>
+				))}
+			</div>
+		</div>
+	);
+};
 
-export default Projects
+export default Projects;
